33a76f042b71d1c87ff7f4cbb620aa07a64543bd
Ottomata <aotto@wikimedia.org>
2021-07-15 10:25:26 -0400

    Lint fixes for Wikimedia

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,6 @@
+/* eslint-env mocha */
+'use strict';
+
 const expect = require('chai').expect;
 
 const RdkafkaPrometheus = require('..');
@@ -6,7 +9,7 @@ describe('RdkafkaPrometheus', () => {
 	describe('metrics', () => {
 		it('indexes metrics by name', () => {
 			const rdkafkaPrometheus = new RdkafkaPrometheus({});
-			Object.keys(rdkafkaPrometheus.metrics).forEach(metricKey => {
+			Object.keys(rdkafkaPrometheus.metrics).forEach((metricKey) => {
 				expect(rdkafkaPrometheus.metrics[metricKey].name).to.be.equal(`rdkafka_${metricKey.toLowerCase()}`);
 			});
 		});
